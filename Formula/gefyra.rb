ec7fb60546af45c05ad966b0f5f05bbc4b3913f6
Robert Stein <robert@blueshoe.de>
2022-03-17 20:53:32 +0100

    initial commit

---
+++ Formula/gefyra.rb
@@ -0,0 +1,8 @@
+class Gefyra < Formula
+  desc     "Gefyra, an open-source Kubernetes-based software development toolkit."
+  homepage "https://gefyra.dev"
+  license "Apache-2.0"
+
+  url "https://github.com/Schille/gefyra/releases/download/0.6.3/gefyra-0.6.3-darwin-amd64.zip"
+  sha256 "ec436bcb5f2b1a0c50eb108ad0c0b3072c81a11b96e957ff7610d1435eaab473"
+end
