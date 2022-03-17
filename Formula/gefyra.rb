c97050c907a032f52688544d43db69217f56b61a
Robert Stein <robert@blueshoe.de>
2022-03-17 21:18:09 +0100

    update formula

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -1,8 +1,16 @@
 class Gefyra < Formula
-  desc     "Gefyra, an open-source Kubernetes-based software development toolkit."
+  desc "Blazingly-fast :rocket:, rock-solid, local application development :arrow_right: with Kubernetes."
   homepage "https://gefyra.dev"
-  license "Apache-2.0"
-
   url "https://github.com/Schille/gefyra/releases/download/0.6.3/gefyra-0.6.3-darwin-amd64.zip"
   sha256 "ec436bcb5f2b1a0c50eb108ad0c0b3072c81a11b96e957ff7610d1435eaab473"
+  license "Apache-2.0"
+
+
+  def install
+    bin.install "gefyra"
+  end
+
+  test do
+    system "false"
+  end
 end
