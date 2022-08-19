85ebf01d664567933d983d353afc7f92f27ece02
Robert Stein <SteinRobert@users.noreply.github.com>
2022-08-19 17:42:02 +0200

    Update gefyra.rb

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -1,7 +1,7 @@
 class Gefyra < Formula
   desc "Blazingly-fast, rock-solid, local application development with Kubernetes"
   homepage "https://gefyra.dev"
-  url "https://github.com/gefyrahq/gefyra/releases/download/0.9.1/gefyra-0.0.1-darwin-universal.zip"
+  url "https://github.com/gefyrahq/gefyra/releases/download/0.9.1/gefyra-0.9.1-darwin-universal.zip"
   sha256 "44521aed541272ad069673efcdb64649433177ee07913a701acfb80cee54c6fa"
   version "0.9.1"
   license "Apache-2.0"
