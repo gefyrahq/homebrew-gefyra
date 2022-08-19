f2ca8f0097441d2766bf2fae7b083109fe1ffb65
Robert Stein <SteinRobert@users.noreply.github.com>
2022-08-19 17:47:39 +0200

    Update gefyra.rb

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -3,7 +3,6 @@ class Gefyra < Formula
   homepage "https://gefyra.dev"
   url "https://github.com/gefyrahq/gefyra/releases/download/0.9.1/gefyra-0.9.1-darwin-universal.zip"
   sha256 "44521aed541272ad069673efcdb64649433177ee07913a701acfb80cee54c6fa"
-  version "0.9.1"
   license "Apache-2.0"
 
   def install
