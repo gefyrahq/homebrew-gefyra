10b9a17b62b903711614f568eb11b410e73e8776
Daniel Iñigo <dinigo@protonmail.com>
2024-11-27 23:53:57 +0100

    fix(brew-formula): update the sha256 of the package

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -2,7 +2,7 @@ class Gefyra < Formula
   desc "Blazingly-fast, rock-solid, local application development with Kubernetes"
   homepage "https://gefyra.dev"
   url "https://github.com/gefyrahq/gefyra/releases/download/2.2.3/gefyra-2.2.3-darwin-universal.zip"
-  sha256 "2c98df9c9f0375aceb698814bda6ff20ea5c75fd4158a3f82f19e3b400900f5b"
+  sha256 "e7a44280842b2a55069875c873ef1fd4077f086cce934a2d57517ae8690e702e"
   license "Apache-2.0"
 
   def install
