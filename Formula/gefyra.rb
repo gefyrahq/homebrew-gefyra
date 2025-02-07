c1c1a88d5e7e384d1b781308cd3e6cab62d782be
runner <runner@Mac-1738931986774.local>
2025-02-07 13:03:37 +0000

    gefyra 2.2.5

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -1,8 +1,8 @@
 class Gefyra < Formula
   desc "Blazingly-fast, rock-solid, local application development with Kubernetes"
   homepage "https://gefyra.dev"
-  url "https://github.com/gefyrahq/gefyra/releases/download/2.2.4/gefyra-2.2.4-darwin-universal.zip"
-  sha256 "729aa1686758d37d9ce2863c4694ab88f7c10b7c7ac38221d12a103764d260b1"
+  url "https://github.com/gefyrahq/gefyra/releases/download/2.2.5/gefyra-2.2.5-darwin-universal.zip"
+  sha256 "6f5fe6811827a51a82d33cfd1e668533622e6adab8cbac1fbb86911973a3e831"
   license "Apache-2.0"
 
   def install
