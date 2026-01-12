2938563e8cf87aa32dae0019d65b6c385f952f12
Robert Stein <SteinRobert@users.noreply.github.com>
2026-01-12 15:26:50 +0100

    Update SHA256 checksum in gefyra.rb
    Update SHA256 checksum for gefyra version 2.3.2

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -2,7 +2,7 @@ class Gefyra < Formula
   desc "Blazingly-fast, rock-solid, local application development with Kubernetes"
   homepage "https://gefyra.dev"
   url "https://github.com/gefyrahq/gefyra/releases/download/2.3.2/gefyra-2.3.2-darwin-universal.zip"
-  sha256 "b95bf038c00f57048cda8dfd2907f0b059eb755d45b27d58608b17c70da6dd1d"
+  sha256 "4305bf1434576b21a199df7dc9e64a20f208194d44ce1e92bdfb0895ec0323c5"
   license "Apache-2.0"
 
   def install
