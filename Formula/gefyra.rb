0cff75f9bc571d446c4eb2cae9ad0432a3c32df3
Anka <runner@sjc22-bm212-9b4c652c-64bb-48bd-878e-fe2c8848455d-02732ED07E86.local>
2026-06-05 15:06:29 +0000

    gefyra 2.4.7

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -1,8 +1,8 @@
 class Gefyra < Formula
   desc "Blazingly-fast, rock-solid, local application development with Kubernetes"
   homepage "https://gefyra.dev"
-  url "https://github.com/gefyrahq/gefyra/releases/download/2.4.6/gefyra-2.4.6-darwin-universal.zip"
-  sha256 "8c814b637b9a48d1b1754be065d5ef4ba2a5939e71be4553c7e516d34d62e88c"
+  url "https://github.com/gefyrahq/gefyra/releases/download/2.4.7/gefyra-2.4.7-darwin-universal.zip"
+  sha256 "50fae87d48ec0ed0225b4681fc4b047cd11756186c41cd69ff4e28b26776ed49"
   license "Apache-2.0"
 
   def install
